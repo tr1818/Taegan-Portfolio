40443dbcf634e5bf424b72b48230bd4e681e2dc5
Taegan Reiter <2025Reiter.Taegan@fcboe.org>
2022-11-07 14:48:38 -0500

    Added input programs
    q
    help
    i cant get out

---
+++ c/name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	char name [30];
+	printf("Enter the name of your character: ");
+	scanf("%s", &name);
+	printf("Your character's name is %s", name);
+	return 0;
+;
+printf("Enter the name of your character: ");
+scanf("%s", &name);
+printf("Your character's name is %s", name);
+return 0;} 
